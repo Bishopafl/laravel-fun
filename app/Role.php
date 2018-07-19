44bc3f982dfe5bb9c0050423c29ee6676f136d1a
Adam Lopez <Adamf04@gmail.com>
2018-07-19 19:51:31 -0400

    cleaning up this code.  Adding spacing for better readability in preparation for the new code to come

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+	// does inverse of what is in User.php line 46
+    public function users() {
+    	return $this->belongsToMany('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -24,12 +24,13 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+
+    // METHODS
     // Goes to post table, and look for column user ID by default
     // if you want to specify different column, add second parameter
     // like: return $this->hasOne('App\Post', 'the_user_id');
     public function post() {
         return $this->hasOne('App\Post');
-
     }
 
     // one to many relationships
@@ -38,10 +39,21 @@ class User extends Authenticatable
         return $this->hasMany('App\Post');
     }
 
-
     // many to many relationship using pivot tables
     public function roles() {
         return $this->belongsToMany('App\Role');
+
+        // you can customize table names and columns follow format below
+        // -----------------------------------------------------------------
+        // user_roles => defines a different table name other than in migration name
+        // user_id    => this is the foriegn key of user table
+        // role_id    => the other foreign key of roles table
+        // return $this->belongsToMany('App\Role', 'user_roles', 'user_id', 'role_id');
+        // we don't need to customize because we are following naming convention
+        // -----------------------------------------------------------------
+        // 
+        // -----------------------------------------------------------------
+
     }
 
 
